960ae786e9f6e144a5f9b5b140f30d0d011cbb4e
Brandon Dewitt <brandonsdewitt@gmail.com>
2014-07-02 11:23:14 -0600

    setup version to be in Redis class

--- lib/redis-renew.rb
+++ lib/redis-renew.rb
@@ -1,8 +1,2 @@
 require "redis-renew/version"
 require "active_support/cache/redis_renew"
-
-module Redis
-  module Renew
-    # Your code goes here...
-  end
-end
--- lib/redis-renew/version.rb
+++ lib/redis-renew/version.rb
@@ -1,4 +1,4 @@
-module Redis
+class Redis
   module Renew
     VERSION = "0.0.1"
   end
